e498b4ebd82ec88478bb98cc5944ce262a30e30b
healer-07 <investor.jaykhun@gmail.com>
2024-11-11 21:20:05 +0300

    Update homework05.py

--- homework05.py
+++ homework05.py
@@ -1,8 +1,8 @@
 name = 'Healer'
 print(name)
-age = (23)
+age = 23
 print(age)
-new_age = (age + 7)
-print(new_age)
-is_student = 'Ben'
-print(type(is_student))
+age = age + 7
+print(age)
+is_student = (23<30)
+print(is_student)
